5cfe8de4aaece6f3c7a12a9adf935c1de3e0d1e3
Abdulrahman Abdulhakeem <abdulrahman.tabdulhakeem@gmail.com>
2017-02-15 09:05:28 +0100

    done with major features, ready to refractor code

--- js/app.js
+++ js/app.js
@@ -2,13 +2,29 @@
 var questions = [];
 var answer = '';
 var level = 0;
+var earning = 0;
 var interval;
+var choices;
 
 // Executes on page load
 $(document).ready(function() {
-	$.getJSON("https://opentdb.com/api.php?amount=15&type=multiple", function(data) {
-		//console.log(data.results[0].question)
+	//Start the background sound
+	// startSound('background', true);
+
+	//Get the first 5 esasy questions
+	$.getJSON("https://opentdb.com/api.php?amount=5&difficulty=easy", function(data) {
 		questions = data.results;
+	});
+
+	//Get the next 5 medium questions
+	$.getJSON("https://opentdb.com/api.php?amount=5&difficulty=medium", function(data) {
+		questions = $.merge(questions, data.results);
+	});
+
+	//Get the last 5 hard questions
+	$.getJSON("https://opentdb.com/api.php?amount=5&difficulty=hard", function(data) {
+		questions = $.merge(questions, data.results);
+		// console.log(questions);
 		nextQuestion(questions[level]);
 	});
 
@@ -19,6 +35,8 @@ $(document).ready(function() {
 		//if correct answer
 		$('.options').addClass('disabled');
 		if($this.find('span').html() == answer){
+			//Play Right
+			startSound('rightsound', false);
 			$this.fadeToggle().fadeToggle().addClass('disabled correct');
 			level++
 			raiseLevel();
@@ -27,6 +45,8 @@ $(document).ready(function() {
 			}, 2000);			
 		}
 		else{
+			//Play Wrong
+			startSound('wrongsound', false);
 			$this.fadeToggle().fadeToggle().addClass('disabled wrong');
 			$('.options').each(function(){
 				$this = $(this);
@@ -44,22 +64,18 @@ $(document).ready(function() {
 	});
 
 	$('#fifty').click(function() {
-		var second_opt = false;
-
 		$(this).addClass('disabled');
-		$('.options span').fadeOut();
+		fifty();
+	});
 
-		$('.options').each(function(){
-			$this = $(this);
+	$('#walk-away').click(function(){
+		$(this).addClass('disabled');
+		walkAway();
+	});
 
-			if($this.find('span').html() == answer){
-				$this.find('span').fadeToggle();
-			}
-			else if(!second_opt || Math.floor(Math.random()*1)){
-				$this.find('span').fadeToggle();
-				second_opt = true;
-			}
-		});
+	$('#audience').click(function(){
+		$(this).addClass('disabled');
+		audience();
 	});
 });
 
@@ -74,7 +90,7 @@ function countDown(time, $element) {
 
     interval = setInterval(function() {
         var progressBarWidth = (time-elapse) * $element.width() / time;
-        $element.find('div').animate({ width: progressBarWidth }, 500)
+        $element.find('div').animate({ width: progressBarWidth }, 500);
         $('.timer').text(time-elapse);
         if (++elapse > time) {
         	clearInterval(interval);
@@ -89,26 +105,70 @@ function raiseLevel(){
 }
 
 function win(){
+	clearInterval(interval);
 	$('.levels ul').hide();
-	$('.levels span').show().text('You Win!!!');
-	setTimeout(function() {
-		location.reload();
-	}, 2000);
+	$('.levels span').show().text('You Win!!!<br><br><i>Start Again</i>');
 }
 
-function gameOver(){
+function walkAway(){
+	clearInterval(interval);
 	var take_home = 0;
 	$('.options').addClass('disabled');
 
+	if(level > 0){
+		 earning = $(".levels ul li:nth-child(" + (16-level) + ")").html();
+	}
+	console.log(earning);
+	$('.levels ul').hide();
+	$('.levels span').show().html('Game Over!!! <br><br> Walk Away: <br>GP <b>' + earning + '</b><br><br><i>Start Again</>');
+};
+
+function audience(){
+	var highest = Math.floor(Math.random()*50) + 50;
+	// console.log(highest);
+
+	$(".audience").fadeToggle(1000);
+
+	var count = 0;
+	$('.options').each(function(){
+		$this = $(this);
+
+		if($this.find('span').html() == answer){
+			$(".audience ul li:nth-child(" + (++count) + ")").animate({ height:  highest}, 1000)
+		}
+		else {
+			$(".audience ul li:nth-child(" + (++count) + ")").animate({ height:  Math.floor(Math.random()*highest)}, 1000)
+		}
+	});
+};
+
+function fifty(){
+	var second_opt = false;
+	$('.options span').fadeOut();
+
+	$('.options').each(function(){
+		$this = $(this);
+
+		if($this.find('span').html() == answer){
+			$this.find('span').fadeToggle();
+		}
+		else if(!second_opt || Math.floor(Math.random()*1)){
+			$this.find('span').fadeToggle();
+			second_opt = true;
+		}
+	});
+}
+
+function gameOver(){
+	clearInterval(interval);
+	$('.options .lifeline').addClass('disabled');
+
 	if(level > 5){
-		take_home = 1000;
+		earning = 1000;
 	}
 
 	$('.levels ul').hide();
-	$('.levels span').show().html('Game Over!!! <br> Take Home: GP ' + take_home + '<br>Start Again');
-	setTimeout(function() {
-		// location.reload();
-	}, 2000);
+	$('.levels span').show().html('Game Over!!! <br><br> Take Home: <br>GP <b>' + earning + '</b><br><br><i>Start Again</>');
 };
 
 function nextQuestion(set){
@@ -118,14 +178,23 @@ function nextQuestion(set){
 		$('.question p').html(set.question);
 
 		//Get image
-		// $.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles=Abacus&prop=pageimages&format=jsonp&pithumbsize=100", function(data) {
-		// 	console.log(data.thumbnail.source)
-		// });
+		$.ajax({
+			headers: {
+		    	'Access-Control-Allow-Origin' : '*',
+		    },
+		});
+
+
+		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles=Abacus&prop=pageimages&format=json&pithumbsize=100", function(data) {
+			console.log(1);
+		});
 
 		$('.options').removeClass('correct wrong disabled');
-		$('.options span').fadeIn();
+		$('.options span').html('').fadeIn();
+		$(".audience").fadeOut();
 
-		answer = set.correct_answer;
+		var span = $('<span />').html(set.correct_answer);
+		answer = span.html();
 
 		choices = set.incorrect_answers;
 		choices.push(set.correct_answer);
@@ -162,3 +231,17 @@ function shuffleOptions(options) {
 
   return options;
 };
+
+/**
+* Plays a sound via HTML5 through Audio tags on the page
+*
+* @require the id must be the id of an <audio> tag.
+* @param id the id of the element to play
+* @param loop the boolean flag to loop or not loop this sound
+*/
+startSound = function(id, loop) {
+	soundHandle = document.getElementById(id);
+	if(loop)
+		soundHandle.setAttribute('loop', loop);
+	soundHandle.play();
+}
